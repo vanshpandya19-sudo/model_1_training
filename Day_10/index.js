385490a4246c4f5de055314b4b0875243c82347e
vanshpandya19-sudo <vanshpandya19@gmail.com>
2026-06-01 13:17:29 +0530

    Added Day_10 to Day_12

---
+++ Day_10/index.js
@@ -0,0 +1,86 @@
+// let a = [];
+// console.log(a);
+
+// let b = [10, 20, 30, ];
+// console.log(b);
+
+// let c = new Array(10, 20, 30 );
+// console.log(c);
+
+// let a = ["HTML", "CSS","JAVASCRIPT"];
+// console.log(a[0]);
+// console.log(a[1]);
+// console.log(a[2]);
+
+// let a = ["html", "css" , "JS"];
+// let b = a[0];
+// console.log("first element:" + b);
+
+// let a = ["HTML" ,"CSS" ,"JS"];
+// console.log(a.length);
+// let b = a[a.length - 1];
+// console.log(b);
+
+// let a = ["HTML", "CSS", "JS"] ;
+// a[0]= "bootsrap";
+// console.log(a);
+
+// let a = ["HTML", "CSS" , "JS"];
+// a.push("bootrap");
+// a.unshift("web Develoopment");
+// console.log(a);
+
+// let a = ["WEB DEVELOPMENT" , "HTML", "CSS", "JS", "BOOTRAP"];
+
+// a.pop();
+// console.log(a);
+
+// a.shift();
+// console.group(a);
+
+
+// let a = ["WEB DEVELOPMENT" , "HTML", "CSS", "JS", "BOOTRAP"];
+// a.splice(0,2,"dsa", "web bhai");
+// console.log(a);
+
+// let a = ["HTML", "CSS" ,"JS"];
+
+// for(let i=0; i < a.length; i++){
+//     console.log(a[i]);
+// }
+
+// let a = ["HTML", "CSS" ,"JS"];
+// console.log(a.toString());
+// console.log(a);
+// console.log(typeof(a));
+
+// let arr = [10, 20, 30, 40, 50 ];
+// for(let i=0;i<arr.length;i+=2){
+//     let alt = arr[i];
+// }
+// console.log(alt);
+
+// let a = "string";
+// let b = a.toUpperCase();
+// console.log(b);
+
+// let c = a.toLowerCase();
+// console.log(c);
+
+// function greed(name){
+//     console.log("hello "+ name);
+// }
+
+// greed("vansh");
+
+function greed(){
+    console.log("Devansh");
+}
+
+greed();
+
+const greet= function(){
+    return "Hi there";
+};
+
+console.log(greet());
---
+++ Day_12/script.js
@@ -0,0 +1,7 @@
+// let container = document.querySelector("#container");
+// document.getElementById("container").style.width="100px";
+// document.getElementById("container").style.height="100px";
+// document.getElementById("container").style.border="1px solid black";
+const domContainer = document.querySelector('#like_button_container');
+const root = ReactDOM.createRoot(domContainer);
+root.render(e(LikeButton));
